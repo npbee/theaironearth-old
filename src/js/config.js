fcc4929ee7a2cfabe16fbb3f482c7ee4d9257bde
Nick Ball <nicholaspball@gmail.com>
2015-11-11 11:49:56 -0800

    update colors and add about

--- src/js/config.js
+++ src/js/config.js
@@ -2,18 +2,18 @@ export default {
 
     // Exit
     "0": {
-        hue: 32,
+        hue: 36,
         saturation: 50,
-        lightness: 81,
+        lightness: 85,
         strokeColor: 'rgba(144, 92, 90, 0.5)',
         fillColor: 'rgba(144, 92, 90, 0.5)'
     },
 
     // Ghost
     "1": {
-        hue: 30,
-        saturation: 84,
-        lightness: 84,
+        hue: 19,
+        saturation: 50,
+        lightness: 72,
         hitpoints: [89, 150],
         strokeColor: 'rgba(155, 199, 243, 0.5)',
         fillColor: 'rgba(155, 199, 243, 0.5)'
@@ -21,63 +21,63 @@ export default {
 
     // Second Skin
     "2": {
-        hue: 213,
+        hue: 193,
         saturation: 50,
-        lightness: 29,
+        lightness: 38,
         strokeColor: 'rgba(112, 112, 112, 0.5)',
         fillColor: 'rgba(112, 112, 112, 0.5)'
     },
 
     // Reflection
     "3": {
-        hue: 227,
+        hue: 220,
         saturation: 50,
-        lightness: 9,
+        lightness: 28,
         strokeColor: 'rgba(144, 92, 90, 0.5)',
         fillColor: 'rgba(250, 250, 250, 0.5)'
     },
 
     //Farewell
     "4": {
-        hue: 230,
+        hue: 231,
         saturation: 50,
-        lightness: 2,
+        lightness: 14,
         strokeColor: 'rgba(250, 250, 250, 0.5)',
         fillColor: 'rgba(19, 14, 45, 0.4)'
     },
 
     // Stillness
     "5": {
-        hue: 7,
+        hue: 250,
         saturation: 50,
-        lightness: 14,
+        lightness: 79,
         strokeColor: 'rgba(18, 45, 48, 0.5)',
         fillColor: 'rgba(18, 45, 48, 0.5)',
     },
 
     // Young Guns
     "6": {
-        hue: 2,
-        saturation: 23,
-        lightness: 46,
+        hue: 27,
+        saturation: 50,
+        lightness: 76,
         strokeColor: 'rgba(86, 128, 129, 0.5)',
         fillColor: 'rgba(86, 128, 129, 0.5)'
     },
 
     // Innocent
     "7": {
-        hue: 26,
-        saturation: 49,
-        lightness: 67,
+        hue: 32,
+        saturation: 50,
+        lightness: 89,
         strokeColor: 'rgba(119, 167, 204, 0.5)',
         fillColor: 'rgba(119, 167, 204, 0.5)',
     },
 
     // Wake
     "8": {
-        hue: 175,
-        saturation: 17,
-        lightness: 78,
+        hue: 180,
+        saturation: 50,
+        lightness: 96,
         strokeColor: 'rgba(10, 10, 10, 0.5)',
         fillColor: 'rgba(198, 177, 179, 0.5)'
     }
