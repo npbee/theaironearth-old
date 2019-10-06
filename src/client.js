ed721678271c1cc8ab6f5923518a1dfda583a6c4
Nick Ball <nicholaspball@gmail.com>
2019-10-06 13:59:58 -0700

    Add logging and error handling

--- src/client.js
+++ src/client.js
@@ -1,5 +1,15 @@
 import * as sapper from "@sapper/app";
+import log from "loglevel";
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
 
 sapper.start({
   target: document.querySelector("#sapper"),
 });
+
+if (typeof window !== "undefined") {
+  window.log = log;
+
+  log.setDefaultLevel(dev ? "debug" : "error");
+}
