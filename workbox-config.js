662a2cf7cd31014489d2b3f4e569d05484fc7f0f
Nick Ball <nicholaspball@gmail.com>
2019-10-08 06:28:53 -0700

    Use existing hash for service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  "globDirectory": "__sapper__/export",
-  "globPatterns": [
-    "**/*.{html,jpg,css,js,svg,png,webp,json}"
-  ],
-  "swDest": "__sapper__/export/service-worker.js",
-  "swSrc": "./src/service-worker.js"
+  globDirectory: "__sapper__/export",
+  globPatterns: ["**/*.{html,jpg,css,js,svg,png,webp,json}"],
+  swDest: "__sapper__/export/service-worker.js",
+  swSrc: "./src/service-worker.js",
+  dontCacheBustURLsMatching: /\.\w{8}\./,
 };
