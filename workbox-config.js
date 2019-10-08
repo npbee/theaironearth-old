78b7b96922c36a9aad57078571af10b0b3dda341
Nick Ball <nicholaspball@gmail.com>
2019-10-08 06:12:44 -0700

    Try workbox

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "__sapper__/export",
+  "globPatterns": [
+    "**/*.{html,jpg,css,js,svg,png,webp,json}"
+  ],
+  "swDest": "__sapper__/export/service-worker.js",
+  "swSrc": "./src/service-worker.js"
+};
