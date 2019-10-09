92fe8b6951f0bbe223eb485f4d46378b3dc0aa19
Nick Ball <nicholaspball@gmail.com>
2019-10-08 17:53:06 -0700

    Remove workbox

--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  globDirectory: "__sapper__/export",
-  globPatterns: ["**/*.{html,jpg,css,js,svg,png,webp,json}"],
-  swDest: "__sapper__/export/service-worker.js",
-  swSrc: "./src/service-worker.js",
-  dontCacheBustURLsMatching: /\.\w{8}\./,
-};
