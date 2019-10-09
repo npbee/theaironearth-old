9b95dbd08350bda99f5a67ec81a9323b4299a417
Nick Ball <nicholaspball@gmail.com>
2019-10-08 18:58:41 -0700

    Remove storybook

--- src/stories/IconButtonView.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import IconButton from "../components/IconButton.svelte";
-  import Soundcloud from "../components/icons/Soundcloud.svelte";
-  export let children;
-</script>
-
-<IconButton on:click {...$$props}>
-  <Soundcloud />
-</IconButton>
