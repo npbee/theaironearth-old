9e26be6cf9eb0cde738ad686b12660bdd7511e41
Nick Ball <nicholaspball@gmail.com>
2019-10-13 17:47:42 -0700

    Fill in some content

--- src/components/CreditSource.svelte
+++ src/components/CreditSource.svelte
@@ -1,15 +1,22 @@
 <script>
   export let source;
+
+  const sources = Array.isArray(source) ? source : [source];
 </script>
 
-{#if source.link}
-  <a
-    class="decorated-link"
-    rel="noreferrer noopener nofollow"
-    target="_blank"
-    href={source.link}>
-    {source.name}
-  </a>
-{:else}
-  <span>{source.name}</span>
-{/if}
+{#each sources as source, idx}
+  {#if source.link}
+    <a
+      class="source decorated-link"
+      rel="noreferrer noopener nofollow"
+      target="_blank"
+      href={source.link}>
+      {source.name}
+    </a>
+  {:else}
+    <span class="source">{source.name}</span>
+  {/if}
+  {#if idx !== sources.length - 1}
+    <span>,&nbsp;</span>
+  {/if}
+{/each}
