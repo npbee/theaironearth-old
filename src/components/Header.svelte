9eb872c4ac2c238b887e3e5e2fc11d5161bc73d8
Nick Ball <nicholaspball@gmail.com>
2019-10-23 18:02:45 -0700

    Hide routes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,10 +4,10 @@
   import { site } from "../data";
   export let segment;
 
-  function currentClass(segment, path, cls) {
-    if (segment && segment.includes(path)) return `nav-link--current ${cls}`;
-    return cls;
-  }
+  /* function currentClass(segment, path, cls) { */
+  /*   if (segment && segment.includes(path)) return `nav-link--current ${cls}`; */
+  /*   return cls; */
+  /* } */
 </script>
 
 <header class="ctr py-4 mb-10">
@@ -26,7 +26,7 @@
         links={[site.links.bandcamp, site.links.soundcloud, site.links.spotify]} />
     </nav>
   </div>
-  <div>
+  <!--<div>
     <a
       rel="prefetch"
       class={currentClass(segment, 'album', 'nav-link ml-2')}
@@ -39,5 +39,5 @@
       href="/about">
       About
     </a>
-  </div>
+  </div>-->
 </header>
