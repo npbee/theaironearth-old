e88c1f490f08c8e78f1811b901cb0da370b61462
Nick Ball <nicholaspball@gmail.com>
2019-11-17 16:31:00 -0800

    Tweak albums page

--- src/routes/albums.svelte
+++ src/routes/albums.svelte
@@ -10,27 +10,26 @@
     content="See all of the albums by The Air on Earth." />
 </svelte:head>
 
-<section class="container flex flex-wrap justify-around">
+<section class="ctr flex flex-col md:flex-row flex-wrap md:justify-around">
   {#each Object.values(albums) as album}
-    <div class="flex flex-col max-w-sm m-6">
-      <a href={`/album/${album.id}`} class="mb-4">
-        <img src={album.artwork} alt={`Album artwork for ${album.title}`} />
+    <div class="flex flex-col w-full md:max-w-sm mb-24 md:mr-8 md:mb-0">
+      <a href={`/album/${album.id}`} class="mb-6">
+        <img
+          class="rounded-sm shadow"
+          src={album.artwork}
+          alt={`Album artwork for ${album.title}`} />
       </a>
       <time
         dateTime={album.date}
-        class="text-xs text-grey-700 small-caps block">
+        class="text-sm text-grey-700 small-caps block">
         {album.date}
       </time>
-      <div class="flex justify-between">
-        <div>
-          <a href={`/album/${album.id}`} class="hover:underline inline-block">
-            <h2 class="headline text-3xl leading-tight text-grey-800">
-              {album.title}
-            </h2>
-          </a>
-        </div>
-        <Links links={album.links} category="quick" />
-      </div>
+      <a href={`/album/${album.id}`} class="hover:underline inline-block">
+        <h2 class="headline text-3xl leading-none text-grey-800">
+          {album.title}
+        </h2>
+      </a>
+      <Links links={album.links} category="quick" />
     </div>
   {/each}
 </section>
