e35f8acda2d08f968af61447bfef91d2dbc1b598
Nick Ball <nicholaspball@gmail.com>
2019-11-25 19:16:13 -0800

    Fix home page spacing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,8 @@
   <meta name="description" content="Home page for the band The Air on Earth." />
 </svelte:head>
 
-<section class="mt-16 ml-4" style="color: hsla(39, 6%, 40%, 1)">
+<section class="mt-16 ctr" style="color: hsla(39, 6%, 40%, 1)">
   <p class="small-caps text-xs mb-0">New Album</p>
-  <h1 class="text-4xl leading-none mb-2">Good Sport</h1>
+  <h1 class="text-4xl leading-none mb-1">Good Sport</h1>
   <h2 class="text-lg ml-6">Out Soon</h2>
 </section>
