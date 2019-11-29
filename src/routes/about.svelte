0a39991b802ca9ce277864c2888d762a953d63bb
Nick Ball <nicholaspball@gmail.com>
2019-11-28 16:31:16 -0800

    Update about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -13,19 +13,19 @@
 
 <section class="ctr flex flex-col md:flex-row">
   <figure class="mb-4 md:mr-8 md:-mx-6 md:w-3/5" style="max-width: none">
-    <Image src="me" alt="The Air on Earth promotional picture" />
+    <!--<Image src="about" alt="The Air on Earth promotional picture" />-->
+    <img src="img/about.jpg" alt="The Air on Earth promotional picture" />
   </figure>
   <div>
     <h2 class="small-caps">About</h2>
     <h3 class="mb-4 text-2xl leading-none">The Air on Earth</h3>
-    <p class="mb-6 " style="xbackground-color: rgba(255, 255, 255, 0.5)">
-      <em>The Air on Earth</em>
-      is a musical project by Nick Ball. The project attempts a wide range of
-      genres with influences from artists like Explosions and the Sky, Sigur
-      Rós, Radiohead, Jon Hopkins, Bon Iver, and many more. Within minimal
-      arrangments, ambience, and long songs you'll hear delayed guitars, drone-y
-      synths, electronic drums, pianos, and found percussion sounds. The music
-      will age with the person but the project will stay the same.
+    <p class="mb-6 ">
+      The Air on Earth is a musical project by Nick Ball. With no particular
+      genre in mind the music takes on the shape of personal influences built up
+      over time, including Talk Talk, Radiohead, Explosions in the Sky, Sigur
+      Rós, Jon Hopkins, and Bon Iver. You'll hear vocal harmonies, delayed
+      guitars, drone-y synths, pianos, electronic drums, found percussion, and
+      sometimes even trombones. Long songs are not uncommon.
     </p>
     <p class="mb-6">
       Nick was a member of the band
@@ -34,7 +34,7 @@
       <em>All We Grow</em>
       and
       <em>Range of Light</em>
-      . Nick has also contributed to music from artists
+      . Nick has also contributed to albums from artists
       <a href="https://www.luraymusic.com/" class="decorated-link">Luray</a>
       and
       <a href="https://www.aeroflynn.org/" class="decorated-link">Aero Flynn</a>
