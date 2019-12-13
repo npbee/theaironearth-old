4e6b0189830f2e0237b51d90af94c92792b7a4f1
Nick Ball <nicholaspball@gmail.com>
2019-12-12 19:37:04 -0800

    Fix up header links

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -13,7 +13,7 @@
 <header class="ctr py-4 mb-10">
   <div class="flex items-center justify-between flex-wrap">
     <nav class="links">
-      <a href="/" class="logo">
+      <a href="/" class="logo block">
         <h1 class="inline-block w-24">
           <span class="visually-hidden">The Air on Earth</span>
           <Logo />
@@ -25,22 +25,22 @@
         links={[site.links.bandcamp, site.links.soundcloud, site.links.spotify]} />
     </nav>
   </div>
-  <div>
+  <div class="flex md:block justify-between -ml-3 md:ml-0 -mr-3 md:mr-0">
     <a
       rel="prefetch"
-      class={currentClass(segment, 'album', 'nav-link ml-2')}
+      class={currentClass(segment, 'album', 'nav-link inline-flex md:inline py-2 px-3 md:p-0 md:ml-2')}
       href="/albums">
       Albums
     </a>
     <a
       rel="prefetch"
-      class={currentClass(segment, 'track', 'nav-link ml-2')}
+      class={currentClass(segment, 'track', 'nav-link inline-flex md:inline py-2 px-3 md:p-0 md:ml-2')}
       href="/tracks">
       Tracks
     </a>
     <a
       rel="prefetch"
-      class={currentClass(segment, 'about', 'nav-link ml-2')}
+      class={currentClass(segment, 'about', 'nav-link inline-flex md:inline py-2 px-3 md:p-0 md:ml-2')}
       href="/about">
       About
     </a>
