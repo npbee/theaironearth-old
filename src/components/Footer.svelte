19d02995ad51cc83dd7ae7a2b8bc1fc1e79b3d11
Nick Ball <nicholaspball@gmail.com>
2020-01-05 13:42:11 -0800

    WIP

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 
-<footer class="ctr py-4">
+<footer class="ctr py-4 flex justify-center opacity-75">
   <Links
       links={[site.links.bandcamp, site.links.soundcloud, site.links.spotify]} />
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,18 +21,16 @@
 </svelte:head>
 
 <div
-  class="flex flex-col md:flex-row px-8 ctr mx-auto justify-between items-center
+  class="flex flex-col px-8 ctr mx-auto justify-between items-center
   h-full">
-  <div
-    class="relative flex-1 w-full flex items-center md:justify-end md:mr-16 mb-8
-    md:mb-0">
-    <div class="md:max-w-sm w-full">
-      <Cover {album} trackId="recur" />
+  <section class="w-full mb-8 w-full md:max-w-sm flex items-end justify-between">
+    <div>
+      <p class="small-caps text-xs mb-0">New Album</p>
+      <h1 class="text-4xl leading-none mb-0 text-grey-700">Good Sport</h1>
     </div>
-  </div>
-  <section class="flex-1 w-full mt-0 md:mt-16">
-    <p class="small-caps text-xs mb-0">New Album</p>
-    <h1 class="text-4xl leading-none mb-0 text-grey-700">Good Sport</h1>
     <Links links={album.links} />
   </section>
+  <div class="md:max-w-sm w-full">
+    <Cover {album} trackId="recur" />
+  </div>
 </div>
